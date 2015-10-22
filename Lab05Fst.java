233b00e22ef419aaa3aed212d3b980a9367ab19c
unknown <4069522@HS222-15.vtsd.com>
2015-10-22 11:59:54 -0400

    Lab6 m8

---
+++ Lab05Fst.java
@@ -0,0 +1,102 @@
+// Lab05Fst.java
+// This is the student, starting version of Lab 05F which is divided into 8 cells.
+// The first cell -- "Draw Red Horizontal Lines" -- is provided as an example.
+// Students need to complete the other 7 cells on their own.
+
+import java.awt.*;
+import java.applet.*;
+
+public class Lab05Fst extends Applet
+{
+    public void paint(Graphics g)
+    {
+        // Draw Grid
+        Expo.drawLine(g,250,0,250,650);
+        Expo.drawLine(g,500,0,500,650);
+        Expo.drawLine(g,750,0,750,650);
+        Expo.drawLine(g,0,325,1000,325);
+
+        int x1, y1, x2, y2, x, y, r;
+
+        // Draw Red Horizontal Lines
+        Expo.setColor(g,Expo.red);
+        x1 = 0;
+        y1 = 5;
+        x2 = 250;
+        y2 = 5;
+        for (int j = 1; j <= 32; j++)
+        {
+          Expo.drawLine(g,x1,y1,x2,y2);
+            y1 += 10;
+            y2 += 10;
+        }
+
+        // Draw Blue Vertical Lines
+        Expo.setColor(g,Expo.blue);
+        x1 = 250;
+        y1 = 1;
+        x2 = 250;
+        y2 = 325;
+        for (int j = 1; j<=25; j++)
+        {
+            Expo.drawLine(g,x1,y1,x2,y2);
+                x1 += 10;
+                x2 += 10;
+        }
+        
+        // Draw Gray Diagonal Dots
+        Expo.setColor(g,Expo.grey);
+        x1 = 749;
+        y1 = 40;
+        x2 = 750;
+        y2 = 40;
+        for (int j = 1; j<=26; j++)
+        {
+            Expo.drawLine(g,x1,y1,x2,y2);
+                x1 -= 10;
+                y1 += 10;
+                x2 -= 10;
+                y2 += 10;
+        }
+        
+        // Draw Green Concentric Circles
+        Expo.setColor(g,Expo.green);
+        x1 = 875;
+        y1 = 163;
+        r = 12;
+        for (int j = 1; j <= 12; j++)
+        {
+           Expo.drawCircle(g,x1,y1,r);
+                r += 10;  
+        }
+
+        // Draw Purple Concentric Ovals
+        
+
+
+
+
+        // Draw Pink Concentric Arcs
+
+
+
+
+
+        // Draw Brown Concentric Rectangles
+
+
+
+
+
+        // Draw Gold Sphere
+
+
+
+
+
+    }
+}
+
+
+
+
